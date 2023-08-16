c52b2fdee545079614270ed833a3e4c165e12671
Manjushri <manjushri.ug@gmail.com>
2023-08-16 15:34:53 -0400

    Inheritance Exercise

---
+++ Inheritance/src/main/java/frc/robot/DriveBase.java
@@ -0,0 +1,55 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class DriveBase {
+
+    //Instance variables
+    private final double kDefaultDeadband = 0.02;
+    private final double kDefaultMaxOutput = 1.0;
+  
+    protected double m_deadband = kDefaultDeadband;
+    protected double m_maxOutput = kDefaultMaxOutput;
+
+    //Default Constructor
+    public DriveBase(){
+        
+    }
+  
+    //Constructor to instantiate objects of the class
+    public DriveBase(double deadband, double maxOutput){
+      System.out.println("Constructor of drivetrain");
+      this.m_deadband = deadband; 
+      this.m_maxOutput = maxOutput;
+  
+    }
+  
+    //Class method to set the private member variable
+    public void setDeadband(double deadband) {
+      System.out.println("Set Deadband");
+      m_deadband = deadband;
+    }
+  
+    //Class method to set the private member variable
+    public void setMaxOutput(double maxOutput) {
+      System.out.println("Set Maxoutput");
+      m_maxOutput = maxOutput;
+    }
+  
+    //Class method to access the 'Private' instance variable(getter)
+    public double getDeadBand(){
+      return m_deadband;
+    }
+  
+    //Class method to access the 'Private' instance variable(getter)
+    public double getMaxOutput(){
+      return m_maxOutput;
+    }
+
+    public void stopMotor(){
+
+    }
+}
---
+++ Inheritance/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private TankDrive tankDrive;
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+
+    MotorController m_frontLeft = new PWMVictorSPX(1);
+    MotorController m_rearLeft = new PWMVictorSPX(2);
+    MotorControllerGroup m_left = new MotorControllerGroup(m_frontLeft, m_rearLeft);
+    MotorController m_frontRight = new PWMVictorSPX(3);
+    MotorController m_rearRight = new PWMVictorSPX(4);
+    MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
+    tankDrive = new TankDrive(m_left, m_right);
+    tankDrive.setDeadband(0.4);
+    tankDrive.setMaxOutput(0.8);
+    tankDrive.setSpeed(0.5, 0.5);
+    System.out.println("The deadband of the TankDrive"+ ' ' +tankDrive.getDeadBand());
+    System.out.println("The Max OutPut of the TankDrive" + ' ' + tankDrive.getMaxOutput());
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    new Trigger(m_exampleSubsystem::exampleCondition)
+        .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_exampleSubsystem);
+  }
+}
